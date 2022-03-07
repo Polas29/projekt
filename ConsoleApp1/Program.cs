a564889c493ecdc35dc2ed587e2c9255bf4ee8c8
adam.polacek <adam.polacek@VT1-08>
2022-03-07 13:13:38 +0100

    Přidejte soubory projektu.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //vytvoření slovníku který má jako klíč číslo a jako hodnotu text
+            Dictionary<string, string> slovník = new Dictionary<string, string>();
+            Console.WriteLine("kolik chcete čísel");
+            string input = Console.ReadLine();
+            int inputCislo = Convert.ToInt32(input);
+
+            for (int pocitadlo = 0; pocitadlo < inputCislo; pocitadlo++)
+            {
+                Console.WriteLine("zadej jmeno");
+                string sjok1 = Console.ReadLine();
+                Console.WriteLine("zadej tel.číslo");
+                string cislo = Console.ReadLine();
+                slovník.Add(sjok1, cislo);
+            }
+            foreach (var radekSlovniku in slovník)
+            Console.WriteLine($"Klíč: {radekSlovniku.Key}, Hodnota: {radekSlovniku.Value}");
+
+        }
+    }
+}
